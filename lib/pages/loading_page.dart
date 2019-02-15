4f9308e57c04c94e384d441c91c1a3e984b54918
Andrei Corpodeanu <andrei.corpodeanu@gmail.com>
2019-02-15 16:45:51 +0200

    Added Loading animation

--- lib/pages/loading_page.dart
+++ lib/pages/loading_page.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
 
 class LoadingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.white,
-      child: Center(child: CircularProgressIndicator()),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.lightBlue,
+            Colors.blue,
+            Colors.blueAccent[700],
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: Center(
+          child: FlareActor(
+            'assets/OuijaLoadingPage.flr',
+            alignment: Alignment.center,
+            fit: BoxFit.contain,
+            animation: 'Loading',
+          ),
+        ),
+      ),
     );
   }
 }
